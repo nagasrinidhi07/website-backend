93b0e2f1f517ffe7ed79518469de473fc651e219
naga srinidhi <nagasrinidhi07@gmail.com>
2025-06-30 15:49:10 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,13 +1,7 @@
-import mongoose from "mongoose";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(
-      'mongodb+srv://nagasrinidhi:07041001@cluster0.l46hv.mongodb.net/website'
-    );
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error(" MongoDB Connection Error:", error.message);
-    process.exit(1); // Stop the server if DB fails to connect
-  }
-};
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
